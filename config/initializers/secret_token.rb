e99544b468ff669e79602de16cb4c73806529671
ryan armbrust <ryan.armbrust@gmail.com>
2011-07-01 16:16:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaaProject::Application.config.secret_token = 'd20631d61f6f29afaeb1f56a3c79694e2ee77e8f5e7e743048fe9f149373849506442ecbb3fed1d0ecc66c8935a8a0d3449cfe32fd72d7fcd9a2a783c787abef'
